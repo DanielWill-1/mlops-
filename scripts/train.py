e037fc013206157ac66d26e25c7dcbc28788c5bd
Daniel Willson <danielwillson004@gmail.com>
2025-04-04 11:22:42 +0530

    commit 2

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,9 @@
-import pandas as pd
-# Load the cleaned data
+import pickle
+import numpy as np
 
-products = pd.read_csv("products.csv")
-products['Description'] = products['Description'].fillna('Unknown').astype(str)
-products.to_csv("products_clean.csv", index=False)
+with open('xgboost_model.pkl', 'rb') as f:
+    xgb_model = pickle.load(f)
+
+xgb_input = np.array([[40, 24.5, 0.1, 14, 1]])  # Example input
+prob = xgb_model.predict_proba(xgb_input)[0][1]
+print(f"XGBoost probability: {prob}")
